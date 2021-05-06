2841fcac144bbcdb9860a7f8f4aad644d20c1945
Brad Parker <crustydog9@gmail.com>
2021-05-06 12:39:31 +1000

    Add files via upload

--- src/Program.cs
+++ src/Program.cs
@@ -7,6 +7,7 @@ namespace CreateFile
     {
         string path;
         string fileName;
+        string unit;
         long fileSize;
 
         static void Main()
@@ -22,21 +23,28 @@ namespace CreateFile
             program.path = Console.ReadLine();
             if (program.path == "")
             {
-                Restart("Please input a valid path");
+                Restart("Please input a valid path.");
             }
 
             Console.Write("Input File Name: ");
             program.fileName = Console.ReadLine();
             if (program.fileName == "")
             {
-                Restart("Please input a valid file name");
+                Restart("Please input a valid file name.");
             }
 
-            Console.Write("Input File Size (Bytes): ");
-            long.TryParse(Console.ReadLine(), out program.fileSize);
-            if (program.fileSize == 0)
+            Console.Write("Input Unit (B, KB, MB, GB, TB, PB, or EB): ");
+            program.unit = Console.ReadLine();
+            if (program.unit == "")
             {
-                Restart("Please input a valid file size");
+                Restart("Please input a unit.");
+            }
+
+            Console.Write("Input File Size: ");
+            long.TryParse(Console.ReadLine(), out program.fileSize);
+            if (program.fileSize <= 0)
+            {
+                Restart("Please input a valid file size.");
             }
 
             string bs = @"\";
@@ -54,9 +62,33 @@ namespace CreateFile
             {
                 using (var fileStream = new FileStream(path + fileName, FileMode.Create, FileAccess.Write, FileShare.None))
                 {
+                    if (unit == "KB" || unit == "Kb" || unit == "kB" || unit == "kb")
+                    {
+                        fileSize = fileSize * 1024;
+                    } else if (unit == "MB" || unit == "Mb" || unit == "mB" || unit == "mb")
+                    {
+                        fileSize = fileSize * 1048576;
+                    }
+                    else if (unit == "GB" || unit == "Gb" || unit == "gB" || unit == "gb")
+                    {
+                        fileSize = fileSize * 1073741824;
+                    }
+                    else if (unit == "TB" || unit == "Tb" || unit == "tB" || unit == "tb")
+                    {
+                        fileSize = fileSize * 1099511627776;
+                    }
+                    else if (unit == "PB" || unit == "Pb" || unit == "pB" || unit == "pb")
+                    {
+                        fileSize = fileSize * 1125899906842620;
+                    }
+                    else if (unit == "EB" || unit == "Eb" || unit == "eB" || unit == "eb")
+                    {
+                        fileSize = fileSize * 1152921504606850048;
+                    }
+
                     fileStream.SetLength(fileSize);
 
-                    Restart("File created at " + path + fileName);
+                    Restart("File created at " + path + fileName + " with a size of " + fileSize + " Bytes.");
                 }
             }
             catch (UnauthorizedAccessException)
